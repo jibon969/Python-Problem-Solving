ff29066939043c9ad92a92db60965d86a062728e
jibon969 <jayed.swe@gmail.com>
2023-12-13 22:07:44 +0600

    added positive_negative_or_zero

---
+++ 00-programiz/08_positive_negative_or_zero.py
@@ -0,0 +1,10 @@
+
+# Python Program to Check if a Number is Positive, Negative or 0
+
+number = 10
+if number > 0:
+        print("Positive")
+elif number < 0:
+         print("Negative")
+elif number == 0:
+         print("0")
