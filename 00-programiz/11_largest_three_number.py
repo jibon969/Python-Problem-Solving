00d37f55382800ef905af9c0721c4b3de2491dfb
jibon969 <jayed.swe@gmail.com>
2023-12-14 22:10:11 +0600

    added Largest Among Three Numbers

---
+++ 00-programiz/11_largest_three_number.py
@@ -0,0 +1,12 @@
+# Python Program to Find the Largest Among Three Numbers
+
+a =  int(input("Enter your 1st number : "))
+b = int(input("Enter your 2nd number : "))
+c = int(input("Enter your 3rd number : "))
+
+if a > b:
+    print("A is largest number")
+elif b > c:
+    print("B is largest number ")
+elif a < c:
+    print("C is largest number")
