06ad09e7686cbd0846abbf6725b3e627c16fcc68
jibon969 <jayed.swe@gmail.com>
2024-01-03 22:25:12 +0600

    added code

---
+++ 01-pynative/01_calculate_multiplication_sum_two_numbers.py
@@ -0,0 +1,12 @@
+# Calculate the multiplication and sum of two numbers
+"""
+number1 = 20
+number2 = 30
+output: 600
+"""
+number1 = 20
+number2 = 30
+multiplication = number1 * number2
+print(multiplication)
+sum = number1 * number2
+print(sum)
