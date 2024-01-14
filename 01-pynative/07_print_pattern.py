e68e01ceaab55d732eb213eb996b68f650d103d9
jibon969 <jayed.swe@gmail.com>
2024-01-14 23:28:36 +0600

    remove old code

--- 01-pynative/07_print_pattern.py
+++
@@ -1,6 +0,0 @@
-# Print the following pattern
-
-for num in range(5):
-    for i in range(num):
-        print (num, end=" ")
-    print("\n")
--- 01-pynative/08_count_substring.py
+++
@@ -1,5 +0,0 @@
-# Return the count of a given substring from a string
-
-str_x = "Learn Python, how to learn Python"
-cnt = str_x.count("Python")
-print(cnt)
