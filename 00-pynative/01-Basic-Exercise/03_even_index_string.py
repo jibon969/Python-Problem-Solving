46f5d3d285d899786a9f49d6e3e7e8f872a3a726
jibon969 <jayed.swe@gmail.com>
2024-01-18 23:43:43 +0600

    added string that are present

--- 00-pynative/01-Basic-Exercise/03_even_index_string.py
+++ 00-pynative/01-Basic-Exercise/03_even_index_string.py
@@ -1,7 +1,4 @@
 # Print characters from a string that are present at an even index number
-# str = "Hello World !"
-# x = str[0::2]
-# print(x)
 
 my_name = input("Enter your name : ")
 name = list(my_name)
