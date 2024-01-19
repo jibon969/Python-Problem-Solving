8014f16a685236651a1049d812fff63687e6479e
jibon969 <jayed.swe@gmail.com>
2024-01-19 23:43:58 +0600

    added some file

--- 00-pynative/01-Basic-Exercise/03_even_index_string.py
+++ 00-pynative/01-Basic-Exercise/03_even_index_string.py
@@ -1,4 +1,7 @@
-# Print characters from a string that are present at an even index number
+""" 
+Print characters from a string that are 
+present at an even index number
+"""
 
 my_name = input("Enter your name : ")
 name = list(my_name)
