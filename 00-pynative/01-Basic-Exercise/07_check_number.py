e0161551aa955418b0a2be8f5ace9247c03f6ab9
jibon969 <jayed.swe@gmail.com>
2024-01-21 23:35:56 +0600

    added check number

---
+++ 00-pynative/01-Basic-Exercise/07_check_number.py
@@ -0,0 +1,10 @@
+
+# Check if the first and last number of a list is the same
+
+numbers_x = [10, 20, 30, 40, 10]
+x = numbers_x[0]
+y = numbers_x[-1]
+if x == y:
+    print("True")
+else:
+    print("False ")
