f9a71c882fcc5b56ae0938443b759347e1568015
jibon969 <jayed.swe@gmail.com>
2024-02-01 23:57:31 +0600

    added some command

--- 00-pynative/01-Basic-Exercise/12_income_tax.py
+++ 00-pynative/01-Basic-Exercise/12_income_tax.py
@@ -1,8 +1,9 @@
+
 # For example, suppose the taxable income is 45000, and the income tax payable is
 
 income = 45000
 tax_payable = 0
-print("Given income", income)
+print("Given income : ", income)
 
 if income <= 10000:
     tax_payable = 0
@@ -14,4 +15,4 @@ else:
     tax_payable = 10000 * 10 / 100
     tax_payable += (income - 20000) * 20 / 100
 
-print("Total tax to pay is", tax_payable)
+print("Total tax to pay is : ", tax_payable)
