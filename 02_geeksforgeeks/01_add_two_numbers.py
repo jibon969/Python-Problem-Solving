f69f26960a121b9043bf5b39526b89b37a157bc0
jibon969 <jayed.swe@gmail.com>
2024-02-18 23:37:41 +0600

    added two number

---
+++ 02_geeksforgeeks/01_add_two_numbers.py
@@ -0,0 +1,18 @@
+# How to Add Two Numbers in Python – Easy Programs
+
+""" 
+Input: num1 = 5, num2 = 3
+Output: 8
+Input: num1 = 13, num2 = 6
+Output: 19
+"""
+
+num1 = 5
+num2 = 3
+
+# Adding two nos
+sum = num1 + num2
+
+# printing values
+print("Sum of", num1, "and", num2 , "is", sum)
+
