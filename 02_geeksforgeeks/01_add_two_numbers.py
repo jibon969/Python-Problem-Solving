7f0ee2f1e054610a0954d4ff1916cf6c70ea414f
jibon969 <jayed.swe@gmail.com>
2024-02-18 23:40:54 +0600

    added user input

--- 02_geeksforgeeks/01_add_two_numbers.py
+++ 02_geeksforgeeks/01_add_two_numbers.py
@@ -7,12 +7,19 @@ Input: num1 = 13, num2 = 6
 Output: 19
 """
 
-num1 = 5
-num2 = 3
+# num1 = 5
+# num2 = 3
 
-# Adding two nos
-sum = num1 + num2
+# # Adding two number
+# sum = num1 + num2
+# # printing values
+# print("Sum of", num1, "and", num2 , "is", sum)
 
-# printing values
-print("Sum of", num1, "and", num2 , "is", sum)
 
+# Add Two Numbers with User Input ===================================
+number1 = int(input("Enter 1st number : "))
+number2 = int(input("Enter 2nd number : "))
+# Adding two number
+sum = number1 + number2
+# # printing values
+print("Sum of", number1, "and", number2 , "is", sum)
