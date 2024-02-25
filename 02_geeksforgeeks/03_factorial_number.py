06523bbbd9552f5578895457349eca1f966cfb16
jibon969 <jayed.swe@gmail.com>
2024-02-25 23:46:22 +0600

    added some file

--- 02_geeksforgeeks/03_factorial_number.py
+++ 02_geeksforgeeks/03_factorial_number.py
@@ -1,6 +1,10 @@
-def factorial_number(n):
-    n = n * n
-    return n
 
-x = factorial_number(10)
-print(x)
+# factorial of given number
+def factorial(n):
+
+	# single line to find factorial
+	return 1 if (n==1 or n==0) else n * factorial(n - 1) 
+
+# Driver Code
+num = 5
+print("Factorial of",num,"is",factorial(num))
