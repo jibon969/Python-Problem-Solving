4e33b84dfe4e2edb138461dd22b178488f22f0db
jibon969 <jayed.swe@gmail.com>
2024-03-05 23:58:39 +0600

    added some code

--- 02_geeksforgeeks/12_sum_squares.py
+++ 02_geeksforgeeks/12_sum_squares.py
@@ -1,28 +1,6 @@
-# Python3 Program to
-# find sum of square
-# of first n natural
-# numbers
-
-
-# Return the sum of
-# square of first n
-# natural numbers
-
 def squaresum(n):
-
-	# Iterate i from 1
-	# and n finding
-	# square of i and
-	# add to sum.
-	sm = 0
-	for i in range(1, n+1):
-		sm = sm + (i * i)
-
-	return sm
+	return (n * (n + 1) / 2) * (2 * n + 1) / 3
 
 
-# Driven Program
 n = 4
 print(squaresum(n))
-
-# This code is contributed by Nikita Tiwari.*/
