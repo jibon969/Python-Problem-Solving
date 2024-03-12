a650184f41bb9e0df0724a265d6e7a7c392a501d
jibon969 <jayed.swe@gmail.com>
2024-03-12 22:25:58 +0600

    added recursive function

--- 02_geeksforgeeks/01_BasicPrograms/add_two_numbers.py
+++ 02_geeksforgeeks/01_BasicPrograms/add_two_numbers.py
@@ -40,3 +40,21 @@ y = 6
 z = operator.add(x, y)
 print("operator.add method :", z)
 
+
+
+# Define a recursive function to add two numbers
+def add_numbers_recursive(x, y):
+	if y == 0:
+		return x
+	else:
+		return add_numbers_recursive(x + 1, y - 1)
+
+# Take input from the user
+num1 = 32
+num2 = 2
+
+# Call the recursive function to add the two numbers
+result = add_numbers_recursive(num1, num2)
+
+# Print the result
+print("The sum of", num1, "and", num2, "is", result)
