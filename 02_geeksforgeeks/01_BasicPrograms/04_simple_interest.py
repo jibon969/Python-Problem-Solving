86f71bfecd9e9b07ddcb1aec865bac93e22ec17b
jibon969 <jayed.swe@gmail.com>
2024-03-19 23:04:20 +0600

    added some file

--- 02_geeksforgeeks/01_BasicPrograms/04_simple_interest.py
+++ 02_geeksforgeeks/01_BasicPrograms/04_simple_interest.py
@@ -1,1 +1,4 @@
-# Simple interest formula is given by: Simple Interest = (P x T x R)/100 Where, P is the principal amount T is the time and R is the rate
+# Simple interest formula is given by: 
+# Simple Interest = (P x T x R)/100 Where, 
+# P is the principal amount T is the time and R is the rate
+
